033ba286cf94013c87e4a6accda3c0ef846b001e
Michael Lynch <git@mtlynch.io>
2018-02-12 09:40:59 -0500

    Improving dataset log message

--- dummy_file_generator/dataset.py
+++ dummy_file_generator/dataset.py
@@ -9,8 +9,8 @@ def generate(file_generator, total_dataset_bytes):
                                               file_generator.bytes_per_file)
     for i in range(files_to_generate):
         file_generator.next_file()
-        logger.info('File generation is %.1f%% complete',
-                    (100.0 * (i + 1)) / files_to_generate)
+        logger.info('Generated %d of %d files - %.1f%% complete',
+                    (i + 1), files_to_generate, (100.0 * (i + 1)) / files_to_generate)
 
 
 def _calculate_file_count(total_dataset_bytes, bytes_per_file):
